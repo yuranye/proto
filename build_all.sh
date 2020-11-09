16fa1c82eafe36ad918049bfec3eaa708694d1ea
Yuri Bohatyuk <yuri@variantlabs.co>
2020-11-09 12:39:23 +0200

    Revert broken events.proto. Change build echo

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,9 @@
 for SERVICE in $(find * -type f -name "*.proto"); do #iterate over services
-  echo "$SERVICE"
   for PROTOFILE in $(find "$SERVICE" -type f -name "*.proto"); do #recursive iterate over service protofiles 
-    echo "$PROTOFILE"
+    echo "$PROTOFILE" "go"
     docker run --rm -v${PWD}:${PWD} -w${PWD} jaegertracing/protobuf:latest --proto_path=${PWD} --go_out=${PWD}/gen/go "$PROTOFILE"
+    
+    echo "$PROTOFILE" "csharp"
     docker run --rm -v${PWD}:${PWD} -w${PWD} jaegertracing/protobuf:latest --proto_path=${PWD} --csharp_out=${PWD}/gen/csharp "$PROTOFILE"
   done
 done
